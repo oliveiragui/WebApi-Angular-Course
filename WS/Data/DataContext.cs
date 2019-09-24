c8e70bf39b138aaaf94e309b21c0681d04911bc8
Guilherme <Guilherme@DESKTOP-SG0H71B>
2019-09-24 09:37:08 -0300

    First Commit

---
+++ WS/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using WS.Models;
+
+namespace WS.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ WS/Models/Value.cs
@@ -0,0 +1,9 @@
+namespace WS.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
